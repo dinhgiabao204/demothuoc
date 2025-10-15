09b0e217566203aa188c17db293e3af94edf39da
dinhgiabao204 <dinhgiabaobeo@gmail.com>
2025-10-15 11:26:46 +0700

    Delete api directory

--- api/cors.php
+++
@@ -1,8 +0,0 @@
-<?php
-// api/cors.php
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
-header("Access-Control-Allow-Origin: $origin");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(204); exit; }
